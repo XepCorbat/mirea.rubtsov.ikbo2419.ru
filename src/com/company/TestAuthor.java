adbc21d8e3f4739fd67dc794a4d31ba1efc79519
Your Name <roman11rr@mail.ru>
2020-09-10 23:38:48 +0300

    https://github.com/XepCorbat/mirea.rubtsov.ikbo2419.ru.git

---
+++ src/com/company/TestAuthor.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class TestAuthor {
+
+    public static void main(String[] args) {
+        Author author = new Author("Ella", "Ella@mail.ru", '╬');
+        System.out.println((author));
+    }
+}
